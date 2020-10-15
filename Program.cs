44eab5da2c182ff11cc2da6b41474a51472de65f
paulndam <59145887+paulndam@users.noreply.github.com>
2020-10-14 21:23:23 -0400

    another one

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Bubble_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Rico!");
+
+            int [] integerValues = {4,2,-1,8,20,14,-16,35, 19, 30, 76, -45, 60};
+            bubbleSort(integerValues);
+            Console.WriteLine(string.Join(" | ", integerValues));
+        }
+
+
+
+
+        public static void bubbleSort<T>(T[] array)where T: IComparable{
+
+            for(int i = 0; i < array.Length; i++){
+                for(int j = 0; j < array.Length - 1; j++){
+
+                    if(array[j].CompareTo(array[j+1])< 0){
+
+                        Swap(array, j, j+1);
+
+                    }
+                }
+
+                
+            }
+           
+        }
+
+        private static void Swap<T>(T[] array, int first, int second)where T: IComparable{
+
+            T temp = array[first];
+            array[first]= array[second];
+            array[second]= temp;
+        }
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
